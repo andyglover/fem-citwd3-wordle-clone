999b5c81c5f6ab1811ea84d09e4526d2b911c537
Andy Glover <andrew.l.glover@gmail.com>
2023-01-11 03:00:05 -0800

    Add starting code from lesson

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+document
+  .querySelector(".tester-input")
+  .addEventListener("keydown", function (event) {
+    // uses the isLetter function from above
+    if (!isLetter(event.key)) {
+      event.preventDefault();
+    }
+  });
