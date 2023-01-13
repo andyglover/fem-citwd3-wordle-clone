dccfb06d700544fa53a11deb154a82e7ba1c26d4
Andy Glover <andrew.l.glover@gmail.com>
2023-01-13 07:47:58 -0800

    add loading state and commit function with correctness checking

--- wordy-masters.js
+++ wordy-masters.js
@@ -6,6 +6,14 @@ async function init() {
   let currentGuess = "";
   let currentRow = 0;
 
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day"); //add ?random=1 for a random word every play
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
+  setLoading(false);
+
+  console.log(word);
+
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       //add letter to the end
@@ -30,6 +38,30 @@ async function init() {
 
     // TODO do all the marking as "correct" "close" "wrong"
 
+    const guessParts = currentGuess.split(""); //splits string into array of letters
+    const map = makeMap(wordParts);
+    console.log(map);
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        //do nothing, we already did it
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        //mark as close
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
     // TODO did they win or lose?
 
     currentRow++;
@@ -43,7 +75,7 @@ async function init() {
 
   document.addEventListener("keydown", function handleKeyPress(event) {
     const action = event.key;
-    console.log(action);
+    // console.log(action);
     if (action === "Enter") {
       commit();
     } else if (action === "Backspace") {
@@ -58,6 +90,24 @@ async function init() {
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
+}
+
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("hidden", !isLoading); //if isLoading==false, add 'hidden' class to loadingDiv
+}
+
+//count how many letters are in solution
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
 
 init();
